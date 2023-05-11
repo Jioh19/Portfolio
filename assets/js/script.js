aaca243dd54630c0c314ce132cf15a774aabb848
Jioh19 <jioh1982@gmail.com>
2023-05-11 15:39:19 -0400

    Inicializacion

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+const carrusel = document.querySelector(".carrusel");
+
+window.onmousedown = (e) => {
+	carrusel.dataset.mouseDownAt = e.clientX;
+};
+
+window.onmousemove = (e) => {
+	if (carrusel.dataset.mouseDownAt == "0") return;
+
+	const mouseDelta = parseFloat(carrusel.dataset.mouseDownAt) - e.clientX,
+		maxDelta = window.innerWidth / 2;
+
+	const percentage = (mouseDelta / maxDelta) * -100,
+		nextPercentage = parseFloat(carrusel.dataset.prevPercentage) + percentage;
+        if(nextPercentage < -200) nextPercentage = -100;
+        if(nextPercentage > 0) nextPercentage = 0;
+	// nextPercentage = Math.min(nextPercentage, 0);
+	// nextPercentage = Math.max(nextPercentage, -100);
+	carrusel.dataset.percentage = nextPercentage;
+	carrusel.style.transform = `translate(${nextPercentage}%, -50%)`;
+
+    for (const image of carrusel.getElementsByClassName("image")) {
+		image.style.objectPosition = `${nextPercentage/2 + 100}% 50%`;
+		console.log(image);
+	}
+
+};
+
+window.onmouseup = () => {
+	carrusel.dataset.mouseDownAt = "0";
+	carrusel.dataset.prevPercentage = carrusel.dataset.percentage;
+};
