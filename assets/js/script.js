05098df5d3be7932c9d9f4891284e4fbf3e85871
Jioh19 <jioh1982@gmail.com>
2023-05-11 20:21:16 -0400

    Version alpha 00.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 const carrusel = document.querySelector(".carrusel");
 
-window.onmousedown = (e) => {
+carrusel.onmousedown = (e) => {
 	carrusel.dataset.mouseDownAt = e.clientX;
 };
 
-window.onmousemove = (e) => {
+carrusel.onmousemove = (e) => {
 	if (carrusel.dataset.mouseDownAt == "0") return;
 
 	const mouseDelta = parseFloat(carrusel.dataset.mouseDownAt) - e.clientX,
@@ -12,26 +12,25 @@ window.onmousemove = (e) => {
 
 	const percentage = (mouseDelta / maxDelta) * -100,
 		nextPercentage = parseFloat(carrusel.dataset.prevPercentage) + percentage;
-        if(nextPercentage < -200) nextPercentage = -100;
-        if(nextPercentage > 0) nextPercentage = 0;
-	// nextPercentage = Math.min(nextPercentage, 0);
-	// nextPercentage = Math.max(nextPercentage, -100);
+	if (nextPercentage < -100) nextPercentage = -100;
+	if (nextPercentage > 0) nextPercentage = 0;
 	carrusel.dataset.percentage = nextPercentage;
 
-    carrusel.animate({
-        transform: `translate(${nextPercentage}%, -50%)`
-    }, {duration: 1200, fill:"forwards"});
+	carrusel.animate(
+		{
+			transform: `translate(${nextPercentage  + 13}%, 0%)`,
+		},
+		{ duration: 1200, fill: "forwards" }
+	);
 
-	//carrusel.style.transform = `translate(${nextPercentage}%, -50%)`;
-
-    for (const image of carrusel.getElementsByClassName("image")) {
-        image.animate({
-            objectPosition: `${100+nextPercentage/2}% center`
-        }, {duration:1200, fill: "forwards"});
-	//	image.style.objectPosition = `${nextPercentage/2 + 100}% 50%`;
-	//	console.log(image);
+	for (const image of carrusel.getElementsByClassName("image")) {
+		image.animate(
+			{
+				objectPosition: `${100 + nextPercentage}% center`,
+			},
+			{ duration: 1200, fill: "forwards" }
+		);
 	}
-
 };
 
 window.onmouseup = () => {
